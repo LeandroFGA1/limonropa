2b4f3a6895c4a8591b2288868a9cc3b65bf7a694
LeandroFGA1 <aucache.leandro@gmail.com>
2025-03-18 16:31:31 -0300

    prueba limon 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ import Router from './router/Router';
 import store, { persistor } from './store/store';
 
 // export const BASE_URL = "https://ecosustentable.azurewebsites.net/";
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:8000/"  // Para desarrollo con Vite
+  : "/"; // En producción, se manejará internamente con Nginx
+
 
 const App = () => {
   return (
